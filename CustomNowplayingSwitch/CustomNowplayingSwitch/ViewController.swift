8cdb1f6c33f9056db219179f842aaf291c562945
Long <62803507+leesuby@users.noreply.github.com>
2022-12-02 13:40:30 +0700

    Universal attribute

--- CustomNowplayingSwitch/CustomNowplayingSwitch/ViewController.swift
+++ CustomNowplayingSwitch/CustomNowplayingSwitch/ViewController.swift
@@ -7,28 +7,41 @@
 
 import UIKit
 
+struct NowPlayingSwitch{
+    static var width : CGFloat = 36
+    static var height : CGFloat = 20
+    static var onTintColor : UIColor = UIColor(rgb: 0x9257DF) //dark purple
+    static var offTintColor : UIColor = UIColor.lightGray
+    static var cornerRadius : CGFloat = 1.0
+    static var padding : CGFloat = 2
+    static var thumbSize : CGSize = CGSize(width: 16, height: 16)
+    static var thumbTintColor : UIColor = UIColor.white
+    static var thumbCornerRadius : CGFloat = 1.0
+    static var animationDuration : Double = 1.0
+    static var defaultStatus : Bool = true
+}
+
 class ViewController: UIViewController {
     
     let customSwitch : CustomSwitch = {
         let customSwitch = CustomSwitch()
         customSwitch.translatesAutoresizingMaskIntoConstraints = false
-        customSwitch.onTintColor = UIColor(rgb: 0x9257DF)
-        customSwitch.offTintColor = UIColor.lightGray
-        customSwitch.cornerRadius = 1.0
+        customSwitch.onTintColor = NowPlayingSwitch.onTintColor
+        customSwitch.offTintColor = NowPlayingSwitch.offTintColor
+        customSwitch.cornerRadius = NowPlayingSwitch.cornerRadius
 
-        customSwitch.padding = 2
-        customSwitch.thumbCornerRadius = 1.0
-        customSwitch.thumbTintColor = UIColor.white
-        customSwitch.thumbSize = CGSize(width: 16, height: 16)
-        customSwitch.animationDuration = 1.0
-        customSwitch.isOn = true
+        customSwitch.padding = NowPlayingSwitch.padding
+        customSwitch.thumbCornerRadius = NowPlayingSwitch.thumbCornerRadius
+        customSwitch.thumbTintColor = NowPlayingSwitch.thumbTintColor
+        customSwitch.thumbSize = NowPlayingSwitch.thumbSize
+        customSwitch.animationDuration = NowPlayingSwitch.animationDuration
+        customSwitch.isOn = NowPlayingSwitch.defaultStatus
         
         return customSwitch
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         initView()
         initConstraint()
@@ -45,8 +58,8 @@ class ViewController: UIViewController {
         customSwitch.translatesAutoresizingMaskIntoConstraints = false
         customSwitch.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
         customSwitch.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 5).isActive = true
-        customSwitch.widthAnchor.constraint(equalToConstant: 36).isActive = true
-        customSwitch.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        customSwitch.widthAnchor.constraint(equalToConstant: NowPlayingSwitch.width).isActive = true
+        customSwitch.heightAnchor.constraint(equalToConstant: NowPlayingSwitch.height).isActive = true
     }
 
 }
