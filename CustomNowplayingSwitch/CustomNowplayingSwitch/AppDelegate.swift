7807ca75dd6896ce158c824b61f90ef3acad8aec
leesuby <62803507+leesuby@users.noreply.github.com>
2022-12-02 12:25:20 +0700

    init file

---
+++ CustomNowplayingSwitch/CustomNowplayingSwitch/AppDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  CustomNowplayingSwitch
+//
+//  Created by LAP15335 on 02/12/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        if #available(iOS 13, *) {
+            return true
+        } else {
+            let vc = ViewController()
+            
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            appDelegate.window?.rootViewController = vc
+            appDelegate.window?.makeKeyAndVisible()
+        }
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    @available(iOS 13.0,*)
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    @available(iOS 13.0,*)
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ CustomNowplayingSwitch/CustomNowplayingSwitch/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  CustomNowplayingSwitch
+//
+//  Created by LAP15335 on 02/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let customSwitch : CustomSwitch = {
+        let customSwitch = CustomSwitch()
+        customSwitch.translatesAutoresizingMaskIntoConstraints = false
+        customSwitch.onTintColor = UIColor(rgb: 0x9257DF)
+        customSwitch.offTintColor = UIColor.lightGray
+        customSwitch.cornerRadius = 1.0
+
+        customSwitch.padding = 2
+        customSwitch.thumbCornerRadius = 1.0
+        customSwitch.thumbTintColor = UIColor.white
+        customSwitch.thumbSize = CGSize(width: 16, height: 16)
+        customSwitch.animationDuration = 1.0
+        customSwitch.isOn = true
+        
+        return customSwitch
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        initView()
+        initConstraint()
+    }
+
+    func initView(){
+        view.backgroundColor = .white
+        customSwitch.delegate = self
+    }
+    
+    func initConstraint(){
+        view.addSubview(customSwitch)
+        customSwitch.translatesAutoresizingMaskIntoConstraints = false
+        customSwitch.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5).isActive = true
+        customSwitch.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 5).isActive = true
+        customSwitch.widthAnchor.constraint(equalToConstant: 36).isActive = true
+        customSwitch.heightAnchor.constraint(equalToConstant: 20).isActive = true
+    
+        
+    }
+
+}
+
+extension ViewController : CustomSwitchDelegate{
+    func customSwitch(TouchUpChangeOnOffStatus status: Bool) {
+        print(status)
+    }
+    
+}
+
+
+
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+}
